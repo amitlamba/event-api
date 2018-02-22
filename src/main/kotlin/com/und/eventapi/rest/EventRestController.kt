b3b391cab152d1ac5ca958d781c873ef7dbc3aa3
Amit Lamba <amitlamba4198@gmail.com>
2018-02-22 10:39:37 +0530

    Added cross origin support.

--- src/main/kotlin/com/und/eventapi/rest/EventRestController.kt
+++ src/main/kotlin/com/und/eventapi/rest/EventRestController.kt
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.*
 import javax.servlet.http.HttpServletRequest
 import javax.validation.Valid
 
+@CrossOrigin
 @RestController
 class EventRestController {
 
