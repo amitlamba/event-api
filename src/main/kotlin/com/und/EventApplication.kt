76bdd861f5b6cfbea83f03512bea54629340b037
shiv <shivprataps@gmail.com>
2018-03-04 17:48:10 +0530

    updated to new kotlin version

--- src/main/kotlin/com/und/EventApplication.kt
+++ src/main/kotlin/com/und/EventApplication.kt
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.context.config.annotation.RefreshScope
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient
-import org.springframework.cloud.netflix.feign.EnableFeignClients
+import org.springframework.cloud.openfeign.EnableFeignClients
 import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
